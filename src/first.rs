b8789a0d65541849ea74cb1c01078ec1e92afc07
Michael Bryan <michaelfbryan@gmail.com>
2016-09-15 04:36:30 +0800

    Added a List.push() method

--- src/first.rs
+++ src/first.rs
@@ -1,8 +1,9 @@
+use std::mem::replace;
 
 
 struct Node {
     elem: i32,
-    next: List,
+    next: Link,
 }
 
 enum Link {
@@ -16,7 +17,18 @@ pub struct List {
 
 
 impl List {
+    /// Create an empty list
     pub fn new() -> Self {
         List {head: Link::Empty}
+    }
+
+    /// Pust a value onto the start of the list
+    pub fn push(&mut self, value: i32) {
+        let new_node = Node{
+            elem: value,
+            next: replace(&mut self.head, Link::Empty),
+        };
+
+        self.head = Link::More(Box::new(new_node));
     }
 }
